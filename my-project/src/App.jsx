2d1c698f0efd510d74769270bef4c9e35b34c702
Karimipour-ErfanKarimipour <programming1402@gmail.com>
2023-04-23 22:56:03 +0430

    Colors improved

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         
         {arr.map((item, index) => (
 
-          <div className='w-2/5 m-auto py-4 border-2 rounded-xl font-bold border-black text-center align-middle text-white' style={{backgroundColor: `#${item}`}} key={index}>#{item}</div>
+          <div className='w-2/5 m-auto py-4 border-2 rounded-xl font-bold border-black text-center align-middle text-white' style={{backgroundColor: `#${item}`}} key={index}> <span className='bg-black bg-opacity-20 px-16 py-1 rounded-xl'>#{item}</span></div>
        
        ))}
 
